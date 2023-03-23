d5a2a580a7b920addc1b33e7385f37f8f112d61a
Esther Agbaje <folasadeagbaje@gmail.com>
2023-03-23 13:09:26 +0000

    feat: create directus helpers

--- lib/directus.ts
+++ lib/directus.ts
@@ -16,3 +16,30 @@ export async function getDirectus() {
   await directus.auth.static(process.env.DIRECUTS_TOKEN);
   return directus;
 }
+
+// CRUD operations for issue collection in Directus
+export async function getIssues() {
+  const directus = await getDirectus();
+  const result = await directus.items('issues').readByQuery({
+    limit: -1,
+    fields: ['title', 'description', 'id'],
+  });
+  return result.data;
+}
+
+export async function updateIssue(issue: IssueData) {
+  const { id, title, description } = issue;
+  const directus = await getDirectus();
+  return directus.items('issues').updateOne(id, { title, description });
+}
+
+export async function deleteIssue(id: number) {
+  const directus = await getDirectus();
+  return directus.items('issues').deleteOne(id);
+}
+
+export async function createIssue(issue: Omit<IssueData, 'id'>) {
+  const { title, description } = issue;
+  const directus = await getDirectus();
+  return directus.items('issues').createOne({ title, description });
+}
